2c6fcf0fdf875f3b4f2845df5155bc2bd4bd41f3
najmulhouda <najmulhouda6@gmail.com>
2024-03-13 23:20:32 +0600

    eror occured

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import "./App.css"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-const App = () => {
-  return <div className="App">Hello world</div>
+const PhotoGallery = () => {
+  const [photos, setPhotos] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/photos",
+        )
+        setPhotos(response.data)
+      } catch (error) {
+        console.error("Error fetching photos:", error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  const isPinkBorder = (albumId, id) => (albumId + id) % 2 === 0
+  const today = new Date().getDay()
+
+  return (
+    <div>
+      {photos
+        .filter(photo => [0, 2, 4, 6].includes(today) && photo.id % 2 !== 0)
+        .map(photo => (
+          <div
+            key={photo.id}
+            style={{
+              display: "inline-block",
+              margin: "10px",
+              border: isPinkBorder(photo.albumId, photo.id)
+                ? "2px solid pink"
+                : "none",
+            }}
+          >
+            <img src={photo.url} alt={photo.title} />
+          </div>
+        ))}
+    </div>
+  )
 }
 
-export default App
+export default PhotoGallery
